1ea1da497d654fe40f6725d322a378321d99f171
RKResident <sabya.github.iitk@gmail.com>
2025-08-08 16:17:28 +0530

    Create costly_summit.cpp

---
+++ costly_summit.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+unordered_map<char, int> map_sort(unordered_map<char, int> m){
+    vector<pair<char, int>> v = {};
+    for (auto p: m){
+        v.push_back(p);
+    };
+    sort(v.begin(), v.end(), [=](pair<char, int> a, pair<char, int> b){
+        return (a.second < b.second);
+    });
+    m = {};
+    for (auto p: v){
+        m[p.first] =  p.second;
+    };
+    return m;
+};
+
+int main() {
+	int t, n, c, to_sub, cost, a;
+	string people;
+	unordered_map<char, int> counts = {{'A', 0}, {'B', 0}, {'C', 0}, {'D', 0}, {'E', 0}};
+	vector<int> sols;
+	cin >> t;
+	
+	for (int i = 0; i < t; i++){
+	    cin >> n >> c;
+	    cin >> people;
+	    to_sub = n;
+	    counts = {{'A', 0}, {'B', 0}, {'C', 0}, {'D', 0}, {'E', 0}};
+	    for (auto elem: people){
+	        counts[elem] += 1;
+	    };
+	    cost = (int) n*(n+1)/2;
+	    counts = map_sort(counts);
+	    for (auto p: counts){
+	        a = p.second;
+	        if (to_sub * a - a*(a-1)/2 > c){
+	            cost = (int) cost + c - (to_sub * a - a*(a-1)/2);
+	            to_sub -= a;
+	        }
+	        else{
+	            break;
+	        }
+	    };
+	    sols.push_back(cost);
+	};
+	
+	for (auto x: sols){
+	    cout << x << "\n";
+	}
+	
+	return 0;
+};
